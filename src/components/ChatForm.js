6d454218456dd67e77eb4adc15dd3f5b364fb7a4
carolbaam <carolinabaam@gmail.com>
2018-04-23 23:59:05 -0500

    chat en tiempo real

--- src/components/ChatForm.js
+++ src/components/ChatForm.js
@@ -9,23 +9,22 @@ class ChatForm extends React.Component{
         const message = this.refs.message.value;
        // console.log(author)
        // console.log(text)
-       /* const socketIOClient = require('socket.io-client');
-        const sailsIOClient = require('sails.io.js');
-        const io = sailsIOClient(socketIOClient);
-        io.sails.url = 'http://216.224.183.21:1339';*/
-       /* io.socket.post('/chat/broadcast',{ roomName: 'myroom',user:user,message:message },
+       
+      this.props.socket.socket.post('/chat/broadcast',{ roomName: 'myroom',user:user,message:message },
         function(data,status){
             console.log(data);
-        })  */
+        })
         this.props.onNewMessage({user: user, message: message});
         this.refs.message.value = '';
+        this.refs.user.value = '';
       }
-  
+    
     
     
     render(){
         return(
-        <form className="ChatForm" onSubmit={this.handleNewMessage}>
+         
+            <form className="ChatForm" onSubmit={this.handleNewMessage}>
         <input type="text" placeholder="user" className="author" ref="user"/>
         <input type="text" placeholder="message..." className="text" ref="message" />
         <input type="submit" value="Send" />
